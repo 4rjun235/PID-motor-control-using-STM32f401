90b5a7e426cc221af0f27bf175c435e63ac13498
4rjun235 <arjunprasade235@gmail.com>
2026-04-18 11:01:18 +0530

    Delete Inc directory

--- Inc/EXTI_PID.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXTI_PID_H_
-
-#define EXTI_PID_H_
-#define SYSCFG_ENR     (0x01U << 14)
-#define PCINT		   (0x02U << 4)
-#define PC13CONFG	   (0x01U << 13)
-
-#endif /* EXTI_PID_H_ */
--- Inc/interrupt_tim4.h
+++
@@ -1,8 +0,0 @@
-#ifndef INTERRUPT_TIM4_H
-#define INTERRUPT_TIM4_H
-
-#define APB_ENR  (0x01U << 2)
-#define EXIT_UIE (0x01U << 0)
-#define CR1_ENR  (0x01U << 0)
-
-#endif
